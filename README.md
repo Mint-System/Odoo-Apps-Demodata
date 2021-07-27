440a799148ac4482b6eb7783521afdac49146ab9
Janik von Rotz <login@janikvonrotz.ch>
2021-07-27 11:17:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Demodata
+
+Collection of demodata related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-demodata.git ./addons/demodata
+```
+
+## Available modules
+
+| Module                  | Summary                                 |
+| ----------------------- | --------------------------------------- |
+| [mail_data](mail_data/) | Configure Odoo Development mail server. |
---
+++ mail_data/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Mail Data",
+
+    'summary': """
+        Configure Odoo Development mail server.
+    """,
+
+    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
+    'website': 'https://www.mint-system.ch',
+    'category': 'Technical',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+
+    'depends': ['base'],
+
+    'data': [
+        'data/fetchmail_server.csv',
+        'data/ir_mail_server.csv'
+    ],
+
+    'installable': True,
+    'application': False,
+}
